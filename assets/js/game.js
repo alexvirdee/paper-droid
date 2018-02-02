750fa69a07cf8a56db445d153780b8849ef443ea
Alexinder Virdee <alex.virdee08@gmail.com>
2018-02-02 08:34:07 -0500

    front end work

---
+++ assets/js/game.js
@@ -0,0 +1,26 @@
+// Draw the canvas 
+function draw() {
+    var canvas = document.getElementById('doodle');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        var img = new Image();
+        img.onload = function() {
+            ctx.drawImage(img, 150, 300, 40, 40);
+        };
+        img.src = "../game-project/assets/images/paperplane.svg";
+
+    }
+}
+
+draw();
+
+
+
+// Score 
+
+
+// Create lives 
+
+
+// Create the game loop
