fdf8e42bf63f336c1b2615630a1471c9ad326d44
Alexinder Virdee <alex.virdee08@gmail.com>
2018-02-03 12:32:57 -0500

    angle of sprite

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,6 @@
-// Draw
+// Draw gameArea object
+// Set the canvas dimensions 
+// start, clear, and stop functions
 var myGameArea = {
 	canvas : document.createElement('canvas'),
 	start: function() {
@@ -7,7 +9,7 @@ var myGameArea = {
 		this.context = this.canvas.getContext('2d');
 		document.body.insertBefore(
 			this.canvas,
-			document.body.childNodes[2]
+			document.body.childNodes[2] // set position of canvas in relation to childNode
 			);
 		this.frameNo = 0;
 		this.interval = setInterval(updateGameArea, 20);
@@ -19,10 +21,12 @@ var myGameArea = {
 		clearInterval(this.interval);
 	}
 }
+
+// set coordinates of plane, size, and load img sprite
 plane = new Paperplane(40, 40, 0, 0, "../paper-droid/assets/images/paperplane.svg") 
 
 
-
+// Sprite constructor for paper airplane 
 function Paperplane(width, height, xPos, yPos, image) {
     this.xPos = xPos;
     this.yPos = yPos;
@@ -39,51 +43,104 @@ function Paperplane(width, height, xPos, yPos, image) {
     // };
     	// image.src = "../paper-droid/assets/images/paperplane.svg";
     	ctx = myGameArea.context;
-    	ctx.drawImage(this.image, 
+    	ctx.drawImage(
+    		this.image, 
     		this.xPos, 
     		this.yPos,
-    		this.width, this.height);
+    		this.width, 
+    		this.height
+    		);
     }  
 
 }
 
-
+// new position method for plane
 Paperplane.prototype.newPos = function() {
 	this.xPos += this.speedX;
 	this.yPos += this.speedY;
 }
 
+// method for having the plane move to the opposite side of canvas upon leaving side
+// xPos, yPos
+Paperplane.prototype.checkPos = function() {
+	if (plane.xPos < 0) {
+		this.xPos = myGameArea.canvas.width - plane.width;
+	} else if (plane.xPos >= myGameArea.canvas.width) {
+		this.xPos = 0;
+	} else if (plane.yPos < 0) {
+		this.yPos = myGameArea.canvas.height - plane.height;
+	} else if (plane.yPos > myGameArea.canvas.height) {
+		this.yPos = 0;
+	}
+
+}
+
+
+
+Paperplane.prototype.shoot = function() {
+
+}
 
 
 // Move
 // keyCodes: Right => 39, left => 37, Up => 38, Back => 40, Spacebar => 32
+// keydown function that will move plane sprite across game canvas
 function move(e) {
     // alert(e.keyCode);
     if (e.which == 39) {
-        plane.speedX += 5;
-        console.log("Move 5 pixels to the right")
+        plane.speedX = 5;
+        // console.log("Move 5 pixels to the right")
     }
     if (e.which == 37) {
-        plane.speedX -= 5;
-        console.log("Move 5 pixels to the left")
+        plane.speedX = -5;
+        // console.log("Move 5 pixels to the left")
     }
     if (e.which == 38) {
-        plane.speedY -= 5;
-        console.log("Move 5 pixels up")
+        plane.speedY = -5;
+        // console.log("Move 5 pixels up")
     }
     if (e.which == 40) {
-        plane.speedY += 5;
-        console.log("Move 5 pixels down")
-    }    
+        plane.speedY = 5;
+        // console.log("Move 5 pixels down")
+    } 
+
+    if (e.which == 32) {
+    	plane.shoot();
+    }   
 
 }
 
 document.onkeydown = move;
 
 
+// event listener for slowing down paper airplane sprite when user keysup
+window.addEventListener("keyup", function(e) {
+	  if (e.which == 39) {
+        plane.speedX = 1;
+        // console.log("Move 5 pixels to the right")
+    }
+    if (e.which == 37) {
+        plane.speedX = -1;
+        // console.log("Move 5 pixels to the left")
+    }
+    if (e.which == 38) {
+        plane.speedY = -1;
+        // console.log("Move 5 pixels up")
+    }
+    if (e.which == 40) {
+        plane.speedY = 1;
+        // console.log("Move 5 pixels down")
+    }   
+}); 
 
+
+
+
+// function that starts up upon html loading
+// calls js game functions
 function updateGameArea() {
 	myGameArea.clear();
+	plane.checkPos();
 	plane.newPos();
 	plane.update();
 }
