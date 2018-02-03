df4b40338aaf236de5d2535e5398cef6955ed0eb
Alexinder Virdee <alex.virdee08@gmail.com>
2018-02-03 18:40:06 -0500

    methods update

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,28 +2,28 @@
 // Set the canvas dimensions 
 // start, clear, and stop functions
 var myGameArea = {
-	canvas : document.createElement('canvas'),
-	start: function() {
-		this.canvas.width = 800;
-		this.canvas.height = 400;
-		this.context = this.canvas.getContext('2d');
-		document.body.insertBefore(
-			this.canvas,
-			document.body.childNodes[2] // set position of canvas in relation to childNode
-			);
-		this.frameNo = 0;
-		this.interval = setInterval(updateGameArea, 20);
-	},
-	clear: function() {
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	},
-	stop: function() {
-		clearInterval(this.interval);
-	}
+    canvas: document.createElement('canvas'),
+    start: function() {
+        this.canvas.width = 800;
+        this.canvas.height = 400;
+        this.context = this.canvas.getContext('2d');
+        document.body.insertBefore(
+            this.canvas,
+            document.body.childNodes[2] // set position of canvas in relation to childNode
+        );
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function() {
+        clearInterval(this.interval);
+    }
 }
 
 // set coordinates of plane, size, and load img sprite
-plane = new Paperplane(35, 35, 0, 0, "../paper-droid/assets/images/paperplane.svg") 
+plane = new Paperplane(35, 35, 0, 0, "../paper-droid/assets/images/paperplane.svg")
 
 
 // Sprite constructor for paper airplane 
@@ -39,82 +39,93 @@ function Paperplane(width, height, xPos, yPos, image) {
     this.image = new Image();
     this.image.src = image;
     this.update = function() {
-    // 	 var img = new Image();
-    // 	img.onload = function() {
-    //     ctx.drawImage(img, xPos, yPos, width, height);
-    // };
-    	// image.src = "../paper-droid/assets/images/paperplane.svg";
-    	ctx = myGameArea.context;
-    	ctx.drawImage(
-    		this.image, 
-    		this.xPos, 
-    		this.yPos,
-    		this.width, 
-    		this.height
-    		);
+        // 	 var img = new Image();
+        // 	img.onload = function() {
+        //     ctx.drawImage(img, xPos, yPos, width, height);
+        // };
+        // image.src = "../paper-droid/assets/images/paperplane.svg";
+        ctx = myGameArea.context;
+        ctx.drawImage(
+            this.image,
+            this.xPos,
+            this.yPos,
+            this.width,
+            this.height
+        );
 
-    	for (var i = 0; i < this.fires.length; i++) 
-    		this.fires[i].update(); 	
-    }  
+        for (var i = 0; i < this.fires.length; i++)
+            this.fires[i].update();
+    }
 
 }
 
 // new position method for plane
 Paperplane.prototype.newPos = function() {
-	this.xPos += this.speedX;
-	this.yPos += this.speedY;
+    this.xPos += this.speedX;
+    this.yPos += this.speedY;
 
-	for (var i = 0; i < this.fires.length; i++) 
-		this.fires[i].newPos();
+    for (var i = 0; i < this.fires.length; i++)
+        this.fires[i].newPos();
 
 }
 
 // method for having the plane move to the opposite side of canvas upon leaving side
 // xPos, yPos
 Paperplane.prototype.checkPos = function() {
-	if (plane.xPos < 0) {
-		this.xPos = myGameArea.canvas.width - plane.width;
-	} else if (plane.xPos >= myGameArea.canvas.width) {
-		this.xPos = 0;
-	} else if (plane.yPos < 0) {
-		this.yPos = myGameArea.canvas.height - plane.height;
-	} else if (plane.yPos > myGameArea.canvas.height) {
-		this.yPos = 0;
-	}
+    if (plane.xPos < 0) {
+        this.xPos = myGameArea.canvas.width - plane.width;
+    } else if (plane.xPos >= myGameArea.canvas.width) {
+        this.xPos = 0;
+    } else if (plane.yPos < 0) {
+        this.yPos = myGameArea.canvas.height - plane.height;
+    } else if (plane.yPos > myGameArea.canvas.height) {
+        this.yPos = 0;
+    }
 
 }
 
-
-function Fire(x, y, dx, dy) {
-	this.x = x;
-	this.y = y;
-	this.dx = dx;
-	this.dy = dy;
+// rotate plane sprite method add to event listener for left and right key press
+Paperplane.prototype.drawRotated = function(context, xPos, yPos) {
+    ctx.save();
+    ctx.translate(this.xPos + this.width / 2, this.yPos + this.height / 2);
+    ctx.rotate(this.angle * Math.PI / 180);
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height, -this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.restore();
 }
 
-Fire.prototype.draw = function() {
-	ctx.beginPath();
-	ctx.fillStyle = "#000";
-	ctx.arc(this.x, this.y, 2, 0, 2*Math.PI);
-	ctx.fill();
-	ctx.closePath();
-}
-
-Fire.prototype.update = function() {
-	this.x += this.dx;
-	this.y += this.dy	
-}
-
-
+// plane method that will add to fires array for bullets 
 Paperplane.prototype.fire = function() {
-	var dx = Math.cos(this.angle);
-	var dy = Math.sin(this.angle);
+    var dx = Math.cos(this.angle);
+    var dy = Math.sin(this.angle);
 
-	var f = new Fire(this.x, this.y, dx, dy);
+    var f = new Fire(this.x, this.y, dx, dy);
 
-	this.fires.push(f);
+    this.fires.push(f);
 }
 
+// Fire bullets constructor
+function Fire(x, y, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+}
+
+
+// Drawing bullets 
+Fire.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = "#000";
+    ctx.arc(this.x, this.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+}
+
+// update trajectory of bullets
+Fire.prototype.update = function() {
+    this.x += this.dx;
+    this.y += this.dy
+}
 
 
 // Move
@@ -128,6 +139,7 @@ function move(e) {
     }
     if (e.which == 37) {
         plane.speedX = -5;
+        plane.drawRotated();
         // console.log("Move 5 pixels to the left")
     }
     if (e.which == 38) {
@@ -137,11 +149,10 @@ function move(e) {
     if (e.which == 40) {
         plane.speedY = 5;
         // console.log("Move 5 pixels down")
-    } 
-    	// fire bullet when spacebar is pressed
+    }
     if (e.which == 32) {
-    	plane.fire();
-    }   
+        plane.fire(); // fire bullet when spacebar is pressed
+    }
 
 }
 
@@ -150,7 +161,7 @@ document.onkeydown = move;
 
 // event listener for slowing down paper airplane sprite when user keysup
 window.addEventListener("keyup", function(e) {
-	  if (e.which == 39) {
+    if (e.which == 39) {
         plane.speedX = 1;
         // console.log("Move 5 pixels to the right")
     }
@@ -165,8 +176,8 @@ window.addEventListener("keyup", function(e) {
     if (e.which == 40) {
         plane.speedY = 1;
         // console.log("Move 5 pixels down")
-    }   
-}); 
+    }
+});
 
 
 
@@ -174,11 +185,8 @@ window.addEventListener("keyup", function(e) {
 // function that starts up upon html loading
 // calls js game functions
 function updateGameArea() {
-	myGameArea.clear();
-	plane.checkPos();
-	plane.newPos();
-	plane.update();
+    myGameArea.clear();
+    plane.checkPos();
+    plane.newPos();
+    plane.update();
 }
-
-
-
