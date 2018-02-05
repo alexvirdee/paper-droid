64aabc4be77e97136fee498f9b2ce33cbd85a0e0
Alexinder Virdee <alex.virdee08@gmail.com>
2018-02-05 18:46:30 -0500

    cleanup

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,151 +14,29 @@ var myGameArea = {
         this.frameNo = 60;
         this.interval = setInterval(updateGameArea, 20);
     },
+    animate: function(loop) {
+    	var rf = (function() {
+    		return window.requestAnimationFrame || 
+    			   window.webkitRequestAnimationFrame ||
+    			   window.mozRequestAnimationFrame ||    			
+    			   window.oRequestAnimationFrame ||
+    			   window.msRequestAnimationFrame ||
+    			   function(cb, el) {
+    			   		window.setTimeout(cb, 1000/60);
+    			   }
+    	})();
+
+    	var l = function() {
+    		loop();
+    		rf(l, this.canvas)
+    	}
+    	rf(l, this.canvas);
+    },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
     stop: function() {
         clearInterval(this.interval);
-    }
-}
-
-// set coordinates of plane, size, and load img sprite
-plane = new Paperplane(35, 35, 400, 200, "../paper-droid/assets/images/paperplane.svg")
-
-// Sprite constructor for paper airplane 
-function Paperplane(width, height, xPos, yPos, image) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.width = width;
-    this.height = height;
-    this.angle = 0;
-    this.fires = [];
-    this.image = new Image();
-    this.image.src = image;
-    this.update = function() {
-        ctx = myGameArea.context;
-        //=========================
-        // ROTATION
-        //=========================
-        ctx.save();
-        ctx.translate(this.xPos + 20, this.yPos + 20);
-        ctx.rotate(this.angle);
-        ctx.drawImage(
-            this.image,
-            this.width / -2,
-            this.height / -2,
-            this.width,
-            this.height
-        );
-        ctx.restore();
-
-        this.fires.forEach(elem => {
-            elem.update();
-        });
-        this.fires.forEach(elem => {
-            elem.draw();
-        });
-    }
-
-}
-
-// new position method for plane
-Paperplane.prototype.newPos = function() {
-    this.xPos += this.speedX;
-    this.yPos += this.speedY;
-}
-
-// method for having the plane move to the opposite side of canvas upon leaving side
-// xPos, yPos
-Paperplane.prototype.checkPos = function() {
-    if (plane.xPos < 0) {
-        this.xPos = myGameArea.canvas.width - plane.width;
-    } else if (plane.xPos >= myGameArea.canvas.width) {
-        this.xPos = 0;
-    } else if (plane.yPos < 0) {
-        this.yPos = myGameArea.canvas.height - plane.height;
-    } else if (plane.yPos > myGameArea.canvas.height) {
-        this.yPos = 0;
-    }
-
-}
-
-// plane firing 
-Paperplane.prototype.fire = function() {
-    var dx = /*Math.cos(this.angle)*/ 0;
-    // console.log(dx);
-    var dy = /*Math.sin(this.angle)*/ 0;
-    // console.log(dy);
-    // bullets move in direction plane is moving 
-    //====================================================================================
-    var f = new Fire(Math.sign(plane.speedX),
-        Math.sign(plane.speedY),
-        this.xPos + this.width,
-        this.yPos, dx, dy
-    );
-    this.fires.push(f);
-}
-
-
-// fire constructor push to fires array in plane fire method
-function Fire(speedX, speedY, x, y, dx, dy) {
-    // speed variables
-    this.speedX = speedX;
-    this.speedY = speedY;
-
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-}
-
-// draw bullets
-Fire.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.fillStyle = "#000";
-    ctx.arc(this.x, this.y, 2, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
-}
-
-// update position of bullets
-Fire.prototype.update = function() {
-    this.x += /*this.dx*/ this.speedX * 6.5;
-    this.y += /*this.dy*/ this.speedY * 6.5;
-}
-
-
-// ***** PAPER TARGETS *****
-target = new Target(80, 100, 100, 150, "../paper-droid/assets/images/method-draw-image.svg");
-var targets = [];
-// enemy object constructor 
-function Target(width, height, xPos, yPos, image) {
-
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.angle = 0;
-    this.xPos = xPos;
-    this.yPos = yPos;
-
-    this.image = new Image();
-    this.image.src = image;
-    this.update = function() {
-        ctx = myGameArea.context;
-        ctx.save();
-        ctx.translate(this.xPos + 10, this.yPos + 10);
-        ctx.rotate(this.angle += 0.05);
-        ctx.drawImage(
-            this.image,
-            this.width / -2,
-            this.height / -2,
-            this.width,
-            this.height
-        );
-        ctx.restore();
     }
 }
 
